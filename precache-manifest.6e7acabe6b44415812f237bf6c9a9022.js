3d301ce8e3facf135c8f027ff5c579a6dc3d29e2
Tanya <fullstackon@gmail.com>
2020-05-18 17:26:35 +0300

    Updates

---
+++ precache-manifest.6e7acabe6b44415812f237bf6c9a9022.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729498f2534a4dde79b798de29e39ef3",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "a7bac7baa4f73b95dd9a",
+    "url": "/react/static/css/main.eb397c4a.chunk.css"
+  },
+  {
+    "revision": "9e009b9bd84549bbaaaf",
+    "url": "/react/static/js/2.fe9350df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.fe9350df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7bac7baa4f73b95dd9a",
+    "url": "/react/static/js/main.7e3ef031.chunk.js"
+  },
+  {
+    "revision": "794436a6088d848ef866",
+    "url": "/react/static/js/runtime-main.8874d199.js"
+  }
+]);
